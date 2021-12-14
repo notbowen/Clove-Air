153915bbe9170e115a5efaa1f75311926a64ac3b
Hu Bowen <85286288+wHo69@users.noreply.github.com>
2021-12-14 12:47:12 +0000

    changed image

--- cogs/help/help.py
+++ cogs/help/help.py
@@ -14,7 +14,7 @@ class HelpCommand(commands.Cog):
     @commands.command()
     async def help(self, ctx):
         embed=discord.Embed(title="Help", color=await generateRandomColor())
-        embed.set_footer(text="**RoAv Helper**", icon_url="https://cdn.discordapp.com/attachments/739830479330476118/919815855645278298/logo2.png")
+        embed.set_footer(text="Clove Air", icon_url="https://cdn.discordapp.com/attachments/920180147850723339/920295776469528586/logo.jpg")
         embed.add_field(name="Help", value="Shows this embed.", inline=False)
 
         await ctx.send(embed=embed)
